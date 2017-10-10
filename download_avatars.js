22dacba3c848ad0de85a274de36b16642542697e
Travis-ko <traviskothlow@gmail.com>
2017-10-10 06:23:25 +0000

    step 10 commit

--- download_avatars.js
+++ download_avatars.js
@@ -3,6 +3,10 @@ var fs = require("fs");
 var GITHUB_USER = "Travis-ko";
 var GITHUB_TOKEN = "44d66c6708dd505da78db45e06a67d3926ca03e8";
 var filePath = "avatars/";
+var owner = process.argv[2];
+var repo = process.argv[3];
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -19,7 +23,12 @@ function downloadImageByURL(url, filePath) {
     request.get(url)
         .pipe(fs.createWriteStream(filePath));
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(owner, repo, function(err, result) {
+  if (typeof repo !== 'string'){
+    console.log("Error: please input valid owner and repo");
+  }
+  else{
     var bodyObject = JSON.parse(result.body);
     bodyObject.forEach((item) => downloadImageByURL(item.avatar_url, filePath + item.login + ".jpg"));
+  }
 });
