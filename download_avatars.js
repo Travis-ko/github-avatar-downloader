9b75a7336a5e3a5eacff5d2b8289a4e56ce0581e
Travis-ko <traviskothlow@gmail.com>
2017-10-10 04:34:14 +0000

    commit at step 4 for download avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
-
+var GITHUB_USER = "Travis-ko";
+var GITHUB_TOKEN = "44d66c6708dd505da78db45e06a67d3926ca03e8";
 console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
+}
+getRepoContributors("jquery", "jquery");
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
