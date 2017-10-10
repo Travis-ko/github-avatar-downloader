fc1f2ae81cfd4e7be2ee5c16ee74948f191468a7
Travis-ko <traviskothlow@gmail.com>
2017-10-10 05:36:58 +0000

    successfully download avatar with hard coded values.

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require("fs");
 var GITHUB_USER = "Travis-ko";
 var GITHUB_TOKEN = "44d66c6708dd505da78db45e06a67d3926ca03e8";
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -13,12 +14,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
     // Request for information from github
     request.get(requestOptions, cb);
 }
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-console.log("Errors:", err);
-var resultObj = JSON.parse(result.body);
-resultObj.forEach((item) => console.log(item.avatar_url))
-});
+function downloadImageByURL(url, filePath) {
+request.get(url)
+.pipe(fs.createWriteStream(filePath));
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+// getRepoContributors("jquery", "jquery", function(err, result) {
+// console.log("Errors:", err);
+// var resultObj = JSON.parse(result.body);
+// resultObj.forEach((item) => console.log(item.avatar_url))
+// });
 // getRepoContributors("jquery", "jquery", function(err, result) {
 //   console.log("Errors:", err);
 //   console.log("Result:", result);
